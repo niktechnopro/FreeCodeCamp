0cd2154e6793302d2779341068a6269db14b5870
ciscoscholar <niktechnopro@gmail.com>
2018-09-11 09:16:00 -0400

    slice testing

---
+++ BasicAlgorithms/copyArrayUsingSlice.js
@@ -0,0 +1,8 @@
+function forecast(arr) {
+  // change code below this line
+  let sliced = arr.slice(2,4);
+  return sliced;
+}
+
+// do not change code below this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
