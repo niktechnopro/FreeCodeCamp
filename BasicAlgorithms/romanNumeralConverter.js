3b6e99069f889537f7d00cd154cfce0277dd0959
ciscoscholar <niktechnopro@gmail.com>
2018-09-15 15:48:34 -0400

    updates

---
+++ BasicAlgorithms/romanNumeralConverter.js
@@ -0,0 +1,23 @@
+const conversions = {
+	'1': 'I',
+	'2': 'II',
+	'3': 'III',
+	'4': 'IV',
+	'5': 'V',
+	'6': 'VI',
+	'7': 'VII',
+	'8': 'VIII',
+	'9': 'IX',
+	'10': 'X',
+	'50': 'L',
+	'100': 'C',
+	'500': 'D',
+	'1000': 'M' 
+}
+
+
+function convertToRoman(num) {
+ return num;
+}
+
+convertToRoman(36);
