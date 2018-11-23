d5c718eac370e697d7f7b7ec06be8b1f595d8629
ciscoscholar <niktechnopro@gmail.com>
2018-11-22 20:10:23 -0500

    updated fizzbuzz

--- codingChallenges/fizzbuzz/fizzbuzz.js
+++ codingChallenges/fizzbuzz/fizzbuzz.js
@@ -1,7 +1,21 @@
+// var fizzBuzz = function (){
+//   for (let i =1; i<=100; i++){
+//   	if (i%3 === 0 && i%7 ===0) console.log('fizzbuzz')
+// 	else if(i % 3 === 0) console.log('fizz');
+// 	else if(i % 7 === 0) console.log('buzz');
+// 	else console.log(i);
+//   }
+// }();
+
+
+
+//another better version
 var fizzBuzz = function (){
   for (let i =1; i<=100; i++){
-	if(i % 3 === 0) console.log('fizz');
-	else if(i % 7 === 0) console.log('buzz');
-	else console.log(i);
+    let str = ''
+    if(i % 3 === 0) str += 'fizz';
+    if(i % 7 === 0) str += 'buzz';
+    if(str === '') str += i;
+    console.log(str);
   }
 }();
